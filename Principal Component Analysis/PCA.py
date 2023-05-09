6c6b73f8275ebd9f9df61a92147ece0da72f5589
chernchao <98671303+cct99@users.noreply.github.com>
2023-05-08 22:04:00 -0400

    Create PCA.py

---
+++ Principal Component Analysis/PCA.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+from sklearn import datasets
+import numpy as np
+from numpy import mean
+from numpy import cov
+from numpy.linalg import eig
+from sklearn.decomposition import PCA
+from matplotlib.pyplot import figure, show
+from matplotlib.ticker import MaxNLocator
+
+import tensorflow as tf
+from tensorflow import keras
